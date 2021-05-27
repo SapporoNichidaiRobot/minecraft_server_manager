0e5c951dfe220da85a12df6c92127f54e465772e
ONC-HNIW <onc.gi@nnkk.work>
2021-05-27 10:32:43 +0900

    update

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -4,9 +4,9 @@ MIN_RAM="10G"
 MAX_RAM="14G"
 
 if [ $# -ne 1 ]; then
-  echo "please set target world." 1>&2
+  echo -e "\033[31mERROR:please set target world.\033[0m" 1>&2
   exit 1
 fi
 
-cd "$ORG_PATH/servers/$1" || exit
-java -Xms$MIN_RAM -Xmx$MAX_RAM -jar "$ORG_PATH/servers/$1/server.jar" nogui
+cd "$ORG_PATH/servers/$1" || exit 1
+java -Xms$MIN_RAM -Xmx$MAX_RAM -jar server.jar nogui
